6cca6e41dffe721b5edc2ab09b912147b5757e0d
Mariyam Ali <mariyam.nahra@gmail.com>
2026-05-23 19:05:45 +0500

    Refactoring in analysis agent

--- backend/analysis_agent.py
+++ backend/analysis_agent.py
@@ -31,14 +31,23 @@ def extract_summary(text):
 
 
 def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None, plots_list=None):
-    start_time = time.time()
-
     output_file_path = "data/history/scenario_analysis_outputs"
+    
+    start_time = time.time()
+    def execution_time():
+        return round((time.time() - start_time), 2)
 
-    analysis_intent = get_analysis_type(user_query, plots_list)
+    def error_response(msg):
+        return {"reply": msg, 
+                "summary": None, 
+                "report": None, 
+                "error_flag": 1, 
+                "agent_execution_time": execution_time()
+                }
 
+    analysis_type = get_analysis_type(user_query, plots_list)
 
-    if analysis_intent == "visual_report":
+    if analysis_type == "visual_report":
         try:
             output_pdf=f'{output_file_path}/Analysis_Visual_Report_{timestamp}.pdf'
             figures_list = plot_wrapper(plots_list, input_file, output_pdf)
@@ -51,18 +60,11 @@ def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None
                 "agent_execution_time": agent_execution_time,
             }
         except Exception as e:
-            print("Error in visual report generation:", e)
-            agent_execution_time = round((time.time() - start_time), 2)
-            return {
-                "reply": f"❌ Failed to generate visual report.",
-                "summary": None,
-                "report": None,
-                "error_flag": 1,
-                "agent_execution_time": agent_execution_time
-            }
+            print("Error in visual_report generation:", e)
+            return error_response("❌ Failed to generate visual_report.")
         
 
-    elif analysis_intent == "mini_report":
+    elif analysis_type == "mini_report":
         # If user wants a general analysis, we provide the entire scenario summary as context, and ask for broad insights, trends, and explanations.
         try:
             _, scenario_summary = build_scenario_summary(input_file)
@@ -77,18 +79,11 @@ def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None
                 "agent_execution_time": agent_execution_time,
             }
         except Exception as e:
-            print("Error in mini-report generation:", e)
-            agent_execution_time = round((time.time() - start_time), 2)
-            return {
-                "reply": f"❌ Failed to generate analysis.",
-                "summary": None,
-                "report": None,
-                "error_flag": 1,
-                "agent_execution_time": agent_execution_time
-            }
+            print("Error in mini_report generation:", e)
+            return error_response("❌ Failed to generate mini_report.")
         
 
-    elif analysis_intent == "direct_answer":
+    elif analysis_type == "direct_answer":
         # If user asks a specific question, we still provide the scenario summary as context, but prompt the LLM to focus on directly answering the specific query.
         try:
             df, scenario_summary = build_scenario_summary(input_file)
@@ -103,18 +98,10 @@ def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None
                 "agent_execution_time": agent_execution_time,
             }
         except Exception as e:
-            print("Error in direct-answer generation:", e)
-            agent_execution_time = round((time.time() - start_time), 2)
-            return {
-                "reply": f"❌ Failed to generate analysis.",
-                "summary": None,
-                "report": None,
-                "error_flag": 1,
-                "agent_execution_time": agent_execution_time
-            }
-        
+            print("Error in direct_answer generation:", e)
+            return error_response("❌ Failed to generate direct_answer.")
 
-    elif analysis_intent == "visual_direct_report":
+    elif analysis_type == "visual_direct_report":
         # If user asks a specific question and selects plots, we still run the interpretation pipeline to get the analysis, then generate plots, and then compile everything into a PDF report.
         try:
             df, scenario_summary = build_scenario_summary(input_file)
@@ -138,18 +125,11 @@ def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None
                 "agent_execution_time": agent_execution_time,
             }
         except Exception as e:
-            print("Error in direct-answer generation:", e)
-            agent_execution_time = round((time.time() - start_time), 2)
-            return {
-                "reply": f"❌ Failed to generate analysis report.",
-                "summary": None,
-                "report": None,
-                "error_flag": 1,
-                "agent_execution_time": agent_execution_time
-            }
+            print("Error in visual_direct_report generation:", e)
+            return error_response("❌ Failed to generate visual_direct_report.")
     
 
-    elif analysis_intent == "visual_mini_report":
+    elif analysis_type == "visual_mini_report":
         # If user asks a general question and selects plots, we still run the synthesize function to get the analysis, then generate plots, and then compile everything into a PDF report.
         try:
             _, scenario_summary = build_scenario_summary(input_file)
@@ -173,19 +153,12 @@ def run_analysis_agent(user_query, input_file, timestamp=None, chat_history=None
                 "agent_execution_time": agent_execution_time,
             }
         except Exception as e:
-            print("Error in direct-answer generation:", e)
-            agent_execution_time = round((time.time() - start_time), 2)
-            return {
-                "reply": f"❌ Failed to generate analysis report.",
-                "summary": None,
-                "report": None,
-                "error_flag": 1,
-                "agent_execution_time": agent_execution_time
-            }
-        
+            print("Error in visual_mini_report generation:", e)
+            return error_response("❌ Failed to generate visual_mini_report.")
+
     else:
         return {
-                "reply": f"❌ Could not classify analysis intent.",
+                "reply": f"❌ Could not classify analysis type.",
                 "summary": None,
                 "report": None,
                 "error_flag": 1,
